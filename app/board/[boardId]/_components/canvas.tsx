45502bc95e28494b04eb759e8c889b1a647015bb
Maria <mauramaria92@gmail.com>
2024-03-18 09:32:47 +0000

    minor updates

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -1,16 +1,35 @@
-'use client';
-
+"use client";
+import { useState } from "react";
+import { CanvasMode, CanvasState } from "@/types/canvas";
 import { Info } from "./info";
 import { Participants } from "./participants";
 import { Toolbar } from "./toolbar";
+import { useHistory, useCanUndo, useCanRedo } from "@/liveblocks.config";
 
-export const Canvas = ({boardId}: {boardId: string}) => {
-    
-    return (
-        <main className="h-full w-full relative bg-neutral-100 touch-none">
-            <Info boardId={boardId} />
-            <Participants />
-            <Toolbar />
-        </main>
-    )
-}
+export const Canvas = ({ boardId }: { boardId: string }) => {
+  const [canvasState, setCanvasState] = useState<CanvasState>({
+    mode: CanvasMode.None,
+  });
+
+  const history = useHistory();
+  const canRedo = useCanRedo();
+  const canUndo = useCanUndo();
+
+
+  return (
+    <main className="h-full w-full relative bg-neutral-100 touch-none">
+      <Info boardId={boardId} />
+      <Participants />
+      <Toolbar
+        canvasState={canvasState}
+        setCanvasState={setCanvasState}
+        undo={() => history.undo()}
+        redo={() => history.redo()}
+        clearAll={() => history.clear()}
+        canUndo={canUndo}
+        canRedo={canRedo}
+        canClearAll={canUndo || canRedo}
+      />
+    </main>
+  );
+};
